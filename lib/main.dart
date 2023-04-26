a3adca34c9156ba5b297347c3a4a84f591578e1d
HastagKc <chaudharykshittiz950@gmail.com>
2023-04-26 06:48:38 +0545

    folder name is change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_app/screens/signupPage/signup_page.dart';
+import 'package:firebase_app/screens/signUpPage/signup_page.dart';
 import 'package:firebase_app/screens/welcomePage/welcome_page.dart';
 import 'package:flutter/material.dart';
 
